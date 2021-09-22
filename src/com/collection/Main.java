01b5849e425b1036503529e56a93ba83b84a326e
juangomez <juancgomez2003@gmail.com>
2021-09-22 03:07:23 -0400

    third commit

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -3,7 +3,7 @@ package com.collection;
 public class Main {
     public static void main(String[] args) {
         for (int i=0;i<5;i++){
-            System.out.println("Java " + i+i);
+            System.out.println("Java juan" + i+i);
         }
     }
 }
